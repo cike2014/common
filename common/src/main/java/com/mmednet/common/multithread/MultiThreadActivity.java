bf2938142aa7126e01a8748a92ed36e5987e8982
debert <1193452522@qq.com>
2016-08-17 11:42:07 +0800

    test  git

---
+++ common/src/main/java/com/mmednet/common/multithread/MultiThreadActivity.java
@@ -0,0 +1,30 @@
+package com.mmednet.common.multithread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mmednet.common.R;
+
+public class MultiThreadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtnAsyncTask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_thread);
+        mBtnAsyncTask = (Button) findViewById(R.id.button_asynctask);
+        mBtnAsyncTask.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.button_asynctask:
+
+                break;
+        }
+    }
+}
