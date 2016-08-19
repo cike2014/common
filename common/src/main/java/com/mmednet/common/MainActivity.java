2543865d930381cab9c38288e6367afea27f1439
debert <1193452522@qq.com>
2016-08-19 17:53:40 +0800

    多线程下载+messenger

--- common/src/main/java/com/mmednet/common/MainActivity.java
+++ common/src/main/java/com/mmednet/common/MainActivity.java
@@ -6,17 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.mmednet.common.download.DownloadActivity;
+import com.mmednet.common.ipc.IPCActivity;
 import com.mmednet.common.multithread.MultiThreadActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mButtonThread;
+    private Button mButtonDownload;
+    private Button mMessenger;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mButtonThread = (Button) findViewById(R.id.button_thread);
+        mButtonDownload = (Button) findViewById(R.id.button_download);
+        mMessenger = (Button) findViewById(R.id.button_messenger);
+
         mButtonThread.setOnClickListener(this);
+        mButtonDownload.setOnClickListener(this);
+        mMessenger.setOnClickListener(this);
     }
 
     @Override
@@ -24,6 +33,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch(v.getId()){
             case R.id.button_thread:
                 startActivity(new Intent(MainActivity.this, MultiThreadActivity.class));
+                break;
+            case R.id.button_download:
+                startActivity(new Intent(MainActivity.this, DownloadActivity.class));
+                break;
+            case R.id.button_messenger:
+                startActivity(new Intent(MainActivity.this, IPCActivity.class));
                 break;
         }
     }
--- common/src/main/java/com/mmednet/common/multithread/MultiThreadActivity.java
+++ common/src/main/java/com/mmednet/common/multithread/MultiThreadActivity.java
@@ -11,6 +11,7 @@ import com.mmednet.common.R;
 public class MultiThreadActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mBtnAsyncTask;
+    private Button mBtnHandlerThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +19,18 @@ public class MultiThreadActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_multi_thread);
         mBtnAsyncTask = (Button) findViewById(R.id.button_asynctask);
         mBtnAsyncTask.setOnClickListener(this);
+        mBtnHandlerThread = (Button) findViewById(R.id.button_handlerthread);
+        mBtnHandlerThread.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.button_asynctask:
-
-                startActivity(new Intent(MultiThreadActivity.this,AsyncTaskActivity.class));
-
+                startActivity(new Intent(MultiThreadActivity.this,ProgressBarTest.class));
+                break;
+            case R.id.button_handlerthread:
+                startActivity(new Intent(MultiThreadActivity.this,ThreadTest.class));
                 break;
         }
     }
